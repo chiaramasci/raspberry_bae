7c3a28242e28b91852a17fde3341eaef819d0aa6
Chiara <chiaramasci98@gmail.com>
2019-02-14 11:49:06 +0100

    added touch_phat and lala.py -> unicorn_phat.py

---
+++ touch_phat/touch_phat.py
@@ -0,0 +1,31 @@
+#to install library
+#curl https://get.pimoroni.com/touchphat  | bash
+
+import touchphat
+import time
+
+def turnonled_all()
+    touchphat.all_on()
+
+def turnoffled_all()
+    touchphat.all_off()
+
+#led can be either a value from 1 to 6 or the pad name
+#pad names: "Back", "A", "B", "C", "D", and "Enter"
+def turnonled(led)
+    touchphat.led_on(led)
+
+def turnoffled(led)
+    touchphat.led_off(led)
+
+while True:
+    for led in range(1,7):
+        touchphat.led_on(led)
+        time.sleep(0.25)
+        touchphat.led_off(led)
+
+#pad can be either a value from 1 to 6 or the pad name
+#pad names: "Back", "A", "B", "C", "D", and "Enter"
+def set_action(pad,func,*args)
+        @touchphat.on_touch(pad)
+            func
